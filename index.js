c99352819f3256a4d96ecff394a58dbf603835c1
emma Vazquez <emmavazquez700@gmail.com>
2022-12-24 15:33:58 -0600

    updated files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,3 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+    });
+  });

7ef29371b122c366e1b5966044a473c1310b06d1
emma Vazquez <emmavazquez700@gmail.com>
2022-12-24 15:38:19 -0600

    updated index.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const {
   deleteUser,
   addFriend,
   deleteFriend,
-} = require("../../controllers/userController");
+} = require("../controllers/userController");
 
 router.route("/").get(getAllUsers).post(createUser);
 
